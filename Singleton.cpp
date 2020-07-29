867167a7c91901d861741ffe971efda642584324
Frank Wan <wanweiok@163.com>
2020-07-29 15:19:19 +0800

    Update Singleton.cpp

--- Singleton.cpp
+++ Singleton.cpp
@@ -1,12 +1,10 @@
-// DesignPattern.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
-#include <iostream>
 #include <assert.h> 
-#include "singleton.h"
+#include "Singleton.h"
+using namespace std;
 
 Singleton* Singleton::_instance = nullptr;
+Singleton::GC Singleton::gc;
 Singleton* Singleton::Instance() {
 	if (_instance == NULL) {
 		_instance = new Singleton();
@@ -14,22 +12,10 @@ Singleton* Singleton::Instance() {
 	return _instance;
 }
 
-
 int main()
 {
 	Singleton* test1 = Singleton::Instance();
 	Singleton* test2 = Singleton::Instance();
 	assert(test1 == test2);
-    std::cout << "Hello World!\n"; 
+	cout << "Hello World!\n"; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
